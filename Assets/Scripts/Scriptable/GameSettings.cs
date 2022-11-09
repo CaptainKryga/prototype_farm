9c373aa401b828e17a6f6993ef872b48d36a16db
CaptainKryga <krygadenis@gmail.com>
2022-11-09 12:27:22 +0200

    upd scriptable's

--- Assets/Scripts/Scriptable/GameSettings.cs
+++ Assets/Scripts/Scriptable/GameSettings.cs
@@ -5,9 +5,9 @@ namespace Scriptable
     [CreateAssetMenu(fileName = "GameSettings", menuName = "ScriptableObjects/GameSettings", order = 1)]
     public class GameSettings : ScriptableObject
     {
-        [Range(2, 20)]
+        [Range(2, 10)]
         public int Width;
-        [Range(2, 20)]
+        [Range(2, 10)]
         public int Height;
     }
 }
