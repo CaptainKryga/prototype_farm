0623ca64b4daec6629f6a007e271d8aca9752541
CaptainKryga <krygadenis@gmail.com>
2022-11-09 14:04:20 +0200

    add gameType's static

---
+++ Assets/Scripts/Static/GameTypes.cs
@@ -0,0 +1,20 @@
+﻿namespace Static
+{
+    public static class GameTypes
+    {
+        public enum Plant
+        {
+            Carrot,
+            Grass,
+            Tree,
+            Close
+        }
+        
+        public enum Level
+        {
+            Min,
+            Mid,
+            Max
+        }
+    }
+}
