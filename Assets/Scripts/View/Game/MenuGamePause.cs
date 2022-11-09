3db9db88331994ac4a8ae6af0eca8384ac57b2d9
CaptainKryga <krygadenis@gmail.com>
2022-11-09 11:06:15 +0200

    upd game ui pause

--- Assets/Scripts/View/Game/MenuGamePause.cs
+++ Assets/Scripts/View/Game/MenuGamePause.cs
@@ -4,6 +4,11 @@ namespace View.Game
 {
     public class MenuGamePause: MenuBase
     {
+        public void ChangePauseState()
+        {
+            SetEnable(!PanelBase.activeSelf);
+        }
+        
         public void OnClick_Continue()
         {
             SetEnable(false);
