9d994c6f2194da4a81ba8ee54a714e47f8d6f9ea
CaptainKryga <krygadenis@gmail.com>
2022-11-09 18:55:48 +0200

    add QualitiSettings, FRAMERATE = 60, VSYNC = 1

---
+++ Assets/Scripts/Utils/QSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    public class QSettings: MonoBehaviour
+    {
+        private void Awake()
+        {
+            Application.targetFrameRate = 60;
+            QualitySettings.vSyncCount = 1;
+        }
+    }
+}
