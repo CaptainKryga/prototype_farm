467b0665e14d4dd7d95c7b418f193a1cc02f525a
CaptainKryga <krygadenis@gmail.com>
2022-11-10 13:47:11 +0200

    add animator and animation's

--- Assets/Scripts/Model/CharaсterFarmer.cs
+++ Assets/Scripts/Model/CharaсterFarmer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using Model.Components;
+using Static;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,44 +10,32 @@ namespace Model
     public class CharapterFarmer : MonoBehaviour
     {
         [SerializeField] private CameraCinematicController CameraCinematicController;
-        // [SerializeField] private CustomAnimator animator;
-        [SerializeField] private Rigidbody rigidbody;
+        [SerializeField] private Animator animator;
 
         [SerializeField] private NavMeshAgent agent;
 
         public bool IsBusy;
-        // [SerializeField] private Transform point1, point2;
-        //
-        // [SerializeField] private GameObject boxSmall, boxMiddle;
-		
-        // private float startSpeed, startAcceleration;
-		      //
-        // public Transform GetTransform { get => worker; }
-        // public Vector3 WorkerBodyStartPosition { get => workerBodyStartPosition; }
-        // public Quaternion WorkerBodyStartRotation { get => workerBodyStartRotation; }
+        private bool _isFarming;
 
         private void Start()
         {
             agent.autoRepath = true;
             agent.autoTraverseOffMeshLink = true;
             agent.autoBraking = true;
-
-            // startSpeed = agent.speed;
-            // startAcceleration = agent.acceleration;
-
-            rigidbody.isKinematic = true;
-			
-            // UpdateAnimation(0, 1);
+            
+            animator.SetFloat(Blend, (int)GameTypes.Anim.Idle);
         }
 
         public bool SetNextPosition(Cell cell, float distance = 2)
         {
+            animator.SetFloat(Blend, (int)GameTypes.Anim.Run);
             IsBusy = true;
             agent.isStopped = false;
             agent.destination = cell.ParentPlant.position;
 
             if (Vector3.Distance(agent.transform.position, cell.ParentPlant.position) <= distance)
             {
+                animator.SetFloat(Blend, (int)GameTypes.Anim.Farming);
                 agent.isStopped = true;
                 agent.velocity = Vector3.zero;
 
@@ -59,7 +48,8 @@ namespace Model
         private Func<PlantData, Cell, int> _saveFunc;
         private PlantData _savePlant;
         private Cell _saveCell;
-        
+        private static readonly int Blend = Animator.StringToHash("Blend");
+
         public void SetNextQuest(Func<PlantData, Cell, int> func, PlantData plant, Cell cell)
         {
             CameraCinematicController.Starter(cell.CamPoint);
@@ -75,6 +65,8 @@ namespace Model
         {
             if (other.GetComponent<Cell>() == _saveCell)
             {
+                animator.SetFloat(Blend, (int)GameTypes.Anim.Farming);
+                
                 agent.isStopped = true;
                 agent.velocity = Vector3.zero;
 
@@ -84,6 +76,10 @@ namespace Model
 
         IEnumerator Delay(float delay, Cell cell)
         {
+            if (_isFarming)
+                yield break;
+
+            _isFarming = true;
             cell.Image.enabled = true;
             cell.Image.color = Color.red;
             
@@ -94,40 +90,13 @@ namespace Model
                 yield return new WaitForEndOfFrame();
             }
 
+            animator.SetFloat(Blend, (int)GameTypes.Anim.Idle);
             cell.Image.enabled = false;
             IsBusy = false;
             CameraCinematicController.Restart();
             _saveFunc(_savePlant, _saveCell);
+            _isFarming = false;
             yield break;
         }
-
-        // public void UpdateAnimation(float walk, float classic)
-        // {
-        //     // animator.UpdateAnimation(walk, classic);
-        // }
-        //
-        // public void UpdateSpeed(float speed)
-        // {
-        //     agent.speed = startSpeed * speed;
-        //     agent.acceleration = startAcceleration * speed * 2;
-        // }
-        //
-        // public Transform GetTransformWorker()
-        // {
-        //     return worker;
-        // }
-
-        // public void UpdateVisibleItem(GameTypes.Item item, bool isFlag)
-        // {
-        //     if (item == GameTypes.Item.BoxSmall)
-        //         boxSmall.SetActive(isFlag);
-        //     else if (item == GameTypes.Item.BoxMiddle)
-        //         boxMiddle.SetActive(isFlag);
-        // }
-        //
-        // public bool CheckActualPath()
-        // {
-        //     return agent.hasPath;
-        // }
     }
 }
